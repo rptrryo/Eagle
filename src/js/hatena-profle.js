b0a875fdb835527c0632771f577fd6ae63bd701e
ryo <rptrryo@gmail.com>
2019-02-11 23:08:55 +0900

    add

---
+++ src/js/hatena-profle.js
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"></link>
+<script type="text/javascript" src="//code.jquery.com/jquery-2.1.3.min.js"></script>
+<script type="text/javascript">
+// <!--
+jQuery.noConflict();
+jQuery(function() {
+  jQuery('.hatena-module-profile a.hatena-id-link').after(
+    ' / <a href="https://twitter.com/rptrryo">' +
+    '<i class="fab fa-twitter-square"></i></a>' +
+    ' / <a href="https://github.com/rptrryo">' +
+    '<i class="fab fa-github-square"></i></a>' +
+    ' / <a href="http://qiita.com/rptrryo">' +
+    '<span class="fa-stack" style="font-size: 50%">' +
+    '<i class="fa fa-square fa-stack-2x"></i>' +
+    '<i class="fa fa-search fa-stack-1x fa-inverse fa-2x"></i>' +
+    '</span></a>'
+  );
+});
+// Qiita icon is adapted from http://qiita.com/hkusu/items/fda8d8178dd693f95f3c
+// -->
+</script>
