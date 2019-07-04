c2c7beb96ea3f9ed02e0d09bfc08a207df2a3f82
ryo <rptrryo@gmail.com>
2019-07-05 01:16:36 +0900

    #2 fix spell miss.

--- src/js/polar-chart4hatena.js
+++ src/js/polar-chart4hatena.js
@@ -6,15 +6,15 @@
     pChartAnimFlag = false;
   
   var fiveLabels = [1, 2, 3, 5, 4];
-  var filveBgColors = ["#33658A", "#86BBD8", "#758E4F", "#F6AE2D", "#F26419"];
+  var fiveBgColors = ["#33658A", "#86BBD8", "#758E4F", "#F6AE2D", "#F26419"];
   var fiveDatas = [3,1,2,2,5];
   var chartTitle = '彼女は頭が悪いから';
 
-  window.addEventListener('load', pChartAnim(fiveLabels, filveBgColors, fiveDatas, chartTitle)); // 読み込み時の処理
-  window.addEventListener('scroll', pChartAnim(fiveLabels, filveBgColors, fiveDatas, chartTitle)); // スクロール時の処理
+  window.addEventListener('load', pChartAnim(fiveLabels, fiveBgColors, fiveDatas, chartTitle)); // 読み込み時の処理
+  window.addEventListener('scroll', pChartAnim(fiveLabels, fiveBgColors, fiveDatas, chartTitle)); // スクロール時の処理
     
   // ポーラーチャート
-  function pChartFunc (fiveLabels, filveBgColors, fiveDatas, chartTitle) {
+  function pChartFunc (fiveLabels, fiveBgColors, fiveDatas, chartTitle) {
 	  var ctx = pChart.getContext('2d'),
 	    chart = new Chart(ctx, {
         type: 'polarArea',
@@ -22,11 +22,11 @@
           labels: [fiveLabels[0], fiveLabels[1], fiveLabels[2], fiveLabels[3], fiveLabels[4]],
           datasets: [{
             backgroundColor: [
-              filveBgColors[0],
-              filveBgColors[1],
-              filveBgColors[2],
-              filveBgColors[3],
-              filveBgColors[4]
+              fiveBgColors[0],
+              fiveBgColors[1],
+              fiveBgColors[2],
+              fiveBgColors[3],
+              fiveBgColors[4]
             ],
             data: [fiveDatas[0],fiveDatas[1],fiveDatas[2],fiveDatas[3],fiveDatas[4]]
           }]
@@ -48,7 +48,7 @@
   };
 
   // スクロール処理
-  function pChartAnim (fiveLabels, filveBgColors, fiveDatas, chartTitle) {
+  function pChartAnim (fiveLabels, fiveBgColors, fiveDatas, chartTitle) {
 	  var wy = window.pageYOffset,
       wb = wy + screen.height - 300, // ウィンドウの最下部位置を取得
       // 各チャートの位置を取得
@@ -56,7 +56,7 @@
   
     // チャートの位置がウィンドウの最下部位置を超えたら起動
 	  if ( wb > pChartPos && pChartAnimFlag == false ) {
-      pChartFunc(fiveLabels, filveBgColors, fiveDatas, chartTitle);
+      pChartFunc(fiveLabels, fiveBgColors, fiveDatas, chartTitle);
       pChartAnimFlag = true;
 	  }
   };
